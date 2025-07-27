51f3218a2f9aeb51864e6b1e73f21388d04a8f05
Oscar Orellana <oscarore284@gmail.com>
2025-07-26 20:38:12 -0600

    feat: shadcn components

---
+++ src/lib/utils.ts
@@ -0,0 +1,11 @@
+import { clsx, type ClassValue } from "clsx"
+import { twMerge } from "tailwind-merge"
+
+export function cn(...inputs: ClassValue[]) {
+  return twMerge(clsx(inputs))
+}
+
+export function formatDate(date: Date): string {
+  console.log("date", date)
+  return date.toLocaleDateString()
+}
